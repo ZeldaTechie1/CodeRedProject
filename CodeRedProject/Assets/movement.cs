6ddeda4b4b8792da5f63036af059c8a850479f6e
aadame3311 <fps3311@gmail.com>
2015-11-22 00:05:43 -0600

    res
    tes

---
+++ CodeRedProject/Assets/movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+    float input_x;
+    float input_z;
+    public float velocity;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        input_x = Input.GetAxisRaw("Horizontal");
+        input_z = Input.GetAxisRaw("Vertical");
+        if (input_x != 0 || input_z != 0)
+        {
+            transform.position += new Vector3(input_x, 0, input_z).normalized * Time.deltaTime * velocity;
+        }
+	}
+}
---
+++ CodeRedProject/Assets/vibrateNearObject.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnlockType = Thalmic.Myo.UnlockType;
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using VibrationType = Thalmic.Myo.VibrationType;
+
+public class vibrateNearObject : MonoBehaviour {
+
+
+    public GameObject myo = null;
+    // public GameObject block;
+    ThalmicMyo thalmicMyo;
+
+    /*
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Pose.Fist
+        thalmicMyo = myo.GetComponent<ThalmicMyo>();   
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            thalmicMyo.Vibrate(VibrationType.Long);
+        }  
+    }
+    */
+
+    void OnCollisionEnter (Collision other)
+    {
+        if (other.gameObject.name == "Cube")
+        {
+            thalmicMyo.Vibrate(VibrationType.Long);
+            Destroy(gameObject);
+            Debug.Log("fghgh");
+
+            ExtendUnlockAndNotifyUserAction(thalmicMyo);
+        }              
+    }
+
+    void ExtendUnlockAndNotifyUserAction(ThalmicMyo myo)
+    {
+        ThalmicHub hub = ThalmicHub.instance;
+
+        if (hub.lockingPolicy == LockingPolicy.Standard)
+        {
+            myo.Unlock(UnlockType.Timed);
+        }
+
+        myo.NotifyUserAction();
+    }
+}
